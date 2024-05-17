e8fa96ae0ffcbe6f629108157d96e88c56efe6a0
Tanush <tanush@myyahoo.com>
2024-05-17 14:04:08 +0530

    common config support added

--- config/config.go
+++ config/config.go
@@ -7,16 +7,20 @@ import (
 	"github.com/spf13/viper"
 )
 
-type Config struct {
-	MODE             string `mapstructure:"MODE"`
-	DB_URL           string `mapstructure:"DB_URL"`
-	HTTPPort         string `mapstructure:"HTTP_PORT"`
-	GRPCPort         string `mapstructure:"GRPC_PORT"`
+type CommonConfig struct {
 	UserGrpc         string `mapstructure:"USER_GRPC"`
 	ImageGrpc        string `mapstructure:"IMAGE_GRPC"`
 	StoreGrpc        string `mapstructure:"STORE_GRPC"`
 	NotificationGrpc string `mapstructure:"NOTIFICATION_GRPC"`
 	ProductGrpc      string `mapstructure:"PRODUCT_GRPC"`
+}
+type Config struct {
+	MODE     string `mapstructure:"MODE"`
+	DB_URL   string `mapstructure:"DB_URL"`
+	HTTPPort string `mapstructure:"HTTP_PORT"`
+	GRPCPort string `mapstructure:"GRPC_PORT"`
+
+	CommonConfig `mapstructure:",squash"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -31,6 +35,22 @@ func LoadConfig() (*Config, error) {
 	if err := viper.Unmarshal(&Config); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
 	}
+
+	viper.SetConfigFile("./config/common-config.yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	if err := viper.Unmarshal(&Config.CommonConfig); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	var commonConfig CommonConfig
+	if err := viper.Unmarshal(&commonConfig); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	Config.CommonConfig = commonConfig
 
 	log.Printf("Config: %+v", Config)
 
